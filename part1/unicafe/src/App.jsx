f467d42d80fd027f89810475a582ee3f0f075db7
Joe Bullard <joekbullard@gmail.com>
2023-06-12 19:28:48 +0100

    unicafe pt1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,34 +1,73 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Header = (props) => <h1>{props.title}</h1>;
 
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+};
+
+// const increaseByOne = ({ setvalue, value }) => {
+//   return setvalue(value + 1)
+// }
+
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+  
+  if (total===0) {
+    return <p>No feedback given</p>;
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <StatisticLine text="Good" count={good} />
+      <StatisticLine text="Neutral" count={neutral} />
+      <StatisticLine text="Bad" count={bad} />
+      <StatisticLine text='Total' count={total} />
+      <StatisticLine text='Average' count={average.toFixed(2)} />
+      <StatisticLine text='Positive' count={positive.toFixed(2)} extra='%' />
     </>
+  )
+}
+
+const StatisticLine = (props) => <p>{props.text}: {props.count} {props.extra}</p>;
+
+const App = () => {
+  // save clicks of each button to its own state
+  const title = 'Give feedback'
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  
+
+  const increaseGoodByOne = () => setGood(good + 1)
+  const increaseNeutralByOne = () => setNeutral(neutral + 1)
+  const increaseBadByOne = () => setBad(bad + 1)
+
+  const total = (good + neutral + bad)
+  const average = (good + bad) / total
+  const positive = (good / total) * 100
+
+  return (
+    <div>
+      <Header title={title} />
+      <Button
+        handleClick={increaseGoodByOne}
+        text='good'
+      />
+      <Button
+        handleClick={increaseNeutralByOne}
+        text='neutral'
+      />
+      <Button
+        handleClick={increaseBadByOne}
+        text='bad'
+      />
+      <h2>Statistics</h2>
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive} />
+    </div>
   )
 }
 
