46fe55c06d109ae240c17e2107cea85d66dea53b
Joe Bullard <joekbullard@gmail.com>
2023-06-13 08:54:56 +0100

    unicafe reformat

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
+
 const Header = ({ title }) => <h1>{title}</h1>;
+
 
 const Button = (props) => {
   return (
@@ -28,7 +30,8 @@ const Statistics = ({ good, neutral, bad, total, average, positive }) => {
       </tbody>
     </table>
   )
-}
+};
+
 
 const StatisticLine = ({ text, count, extra }) => {
   return (
@@ -38,7 +41,8 @@ const StatisticLine = ({ text, count, extra }) => {
       <td>{extra}</td>
     </tr>
   )
-}
+};
+
 
 const App = () => {
   // save clicks of each button to its own state
@@ -46,8 +50,6 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-
 
   const increaseGoodByOne = () => setGood(good + 1)
   const increaseNeutralByOne = () => setNeutral(neutral + 1)
@@ -76,6 +78,6 @@ const App = () => {
       <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive} />
     </div>
   )
-}
+};
 
 export default App
